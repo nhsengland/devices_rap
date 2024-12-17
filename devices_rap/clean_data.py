65105c9482568be1dea28cc91aa18c5e4b6970c5
joseph.wilson8-nhs <joseph.wilson8@nhs.net>
2024-12-17 10:11:08 +0000

    Add function to cleanse taxonomy data for improved data processing

--- devices_rap/clean_data.py
+++ devices_rap/clean_data.py
@@ -24,3 +24,10 @@ def cleanse_provider_data():
     _summary_
     """
     logger.info("Cleaning the master dataset ready for processing")
+
+
+def cleanse_taxonomy_data():
+    """
+    _summary_
+    """
+    logger.info("Cleaning the device taxonomy dataset ready for processing")
