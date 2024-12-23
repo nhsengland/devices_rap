8148b62f0b620dca07dc16c6fae67a35d9595251
joseph.wilson8-nhs <joseph.wilson8@nhs.net>
2024-12-23 18:36:02 +0000

    Implement batch normalization of column names and enhance master data cleansing with error handling

--- devices_rap/clean_data.py
+++ devices_rap/clean_data.py
@@ -2,32 +2,81 @@
 Functions for cleaning and cleansing datasets
 """
 
+from typing import Any, Dict
+
+import pandas as pd
 from loguru import logger
 
+from devices_rap.errors import NoDataProvidedError, NoDatasetsProvidedError
+from devices_rap.utils import (
+    convert_fin_dates,
+    convert_values_to,
+    normalise_column_names,
+)
 
-def cleanse_master_data():
+
+def batch_normalise_column_names(datasets: Dict[str, Dict[str, Any]]) -> Dict[str, Dict[str, Any]]:
     """
-    _summary_
+    Normalise the column names for all datasets in the provided dictionary
+
+    Parameters
+    ----------
+    datasets : Dict[str, Dict[str, Any]]
+        A dictionary containing dataset names and their corresponding dataframes
+
+    Returns
+    -------
+    Dict[str, Dict[str, Any]]
+        The dictionary containing the normalised dataframes
+    """
+    if not datasets:
+        raise NoDatasetsProvidedError("No datasets provided.")
+
+    for name, df in datasets.items():
+        logger.info(f"Normalising column names for the {name} dataset")
+        try:
+            df = datasets[name]["data"]
+            assert isinstance(df, pd.DataFrame)
+            datasets[name]["data"] = normalise_column_names(df)
+        except (KeyError, AssertionError) as e:
+            raise NoDataProvidedError(f"No data provided for the {name} dataset.") from e
+
+    return datasets
+
+
+def cleanse_master_data(master_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Clean the master dataset ready for processing. This function will:
+    - Normalise the column names
+    - Convert high level device type values
+    - Convert activity year values without century
+    - Convert activity date values to datetime
+
+    Parameters
+    ----------
+    master_df : pd.DataFrame
+        The master dataset to be cleaned
+
+    Returns
+    -------
+    pd.DataFrame
+        The cleaned master dataset
     """
     logger.info("Cleaning the master dataset ready for processing")
 
+    logger.info("Converting high level device type values")
+    master_df["upd_high_level_device_type"] = master_df["der_high_level_device_type"].apply(
+        convert_values_to
+    )
 
-def cleanse_exceptions_data():
-    """
-    _summary_
-    """
-    logger.info("Cleaning the master dataset ready for processing")
+    logger.info("Converting activity year values without century")
+    master_df["upd_activity_year"] = master_df["cln_activity_year"].apply(
+        convert_values_to, match=[2425], to=202425
+    )
 
+    logger.info("Converting activity date values to datetime")
+    master_df["activity_date"] = master_df.apply(
+        lambda df: convert_fin_dates(df["cln_activity_month"], df["upd_activity_year"]), axis=1
+    )
 
-def cleanse_provider_data():
-    """
-    _summary_
-    """
-    logger.info("Cleaning the master dataset ready for processing")
-
-
-def cleanse_taxonomy_data():
-    """
-    _summary_
-    """
-    logger.info("Cleaning the device taxonomy dataset ready for processing")
+    return master_df
--- tests/unittests/test_clean_data.py
+++ tests/unittests/test_clean_data.py
@@ -2,12 +2,75 @@
 Tests for devices_rap/clean_data.py
 """
 
+import pandas as pd
 import pytest
 
-# from devices_rap import clean_data
+from devices_rap import clean_data
 
 
 pytestmark = pytest.mark.no_data_needed
+
+
+class TestBatchNormaliseColumnNames:
+    """
+    Tests for clean_data.batch_normalise_column_names
+    """
+
+    def test_checks_for_empty_input(self):
+        """
+        Test that the function raises an error when no data is provided
+        """
+        with pytest.raises(clean_data.NoDatasetsProvidedError):
+            clean_data.batch_normalise_column_names({})
+
+    def test_checks_for_missing_data_key(self, mocker):
+        """
+        Test that the function raises an error when the data key is missing
+        """
+        mocker.patch("devices_rap.clean_data.normalise_column_names")
+        with pytest.raises(clean_data.NoDataProvidedError):
+            clean_data.batch_normalise_column_names({"test": {}})
+
+    def test_check_for_data_not_dataframe(self, mocker):
+        """
+        Test that the function raises an error when the data is not a DataFrame
+        """
+        mocker.patch("devices_rap.clean_data.normalise_column_names")
+        with pytest.raises(clean_data.NoDataProvidedError):
+            clean_data.batch_normalise_column_names({"test": {"data": None}})
+
+    def test_calls_normalise_column_names(self, mocker):
+        """
+        Test that the function calls the normalise_column_names function
+        """
+        mock_function = mocker.patch("devices_rap.clean_data.normalise_column_names")
+        dataframe = pd.DataFrame()
+        datasets = {"test": {"data": dataframe}}
+        clean_data.batch_normalise_column_names(datasets)
+        mock_function.assert_called_once_with(dataframe)
+
+    def test_calls_normalise_column_names_for_each_dataset(self, mocker):
+        """
+        Test that the function calls the normalise_column_names function for each dataset
+        """
+        mock_function = mocker.patch("devices_rap.clean_data.normalise_column_names")
+        input_df = pd.DataFrame()
+        datasets = {"test1": {"data": input_df}, "test2": {"data": input_df}}
+        clean_data.batch_normalise_column_names(datasets)
+        assert mock_function.call_count == 2
+
+    def test_returns_normalised_datasets(self, mocker):
+        """
+        Test that the function returns the normalised datasets
+        """
+        expected_df = pd.DataFrame({"col1": ["test"]})
+        mocker.patch("devices_rap.clean_data.normalise_column_names", return_value=expected_df)
+        input_df = pd.DataFrame()
+        datasets = {"test1": {"data": input_df}, "test2": {"data": input_df}}
+        result = clean_data.batch_normalise_column_names(datasets)
+        assert all("data" in dataset for dataset in result.values())
+        for dataset in result.values():
+            pd.testing.assert_frame_equal(dataset["data"], expected_df)
 
 
 class TestCleanseMasterData:
@@ -15,35 +78,64 @@ class TestCleanseMasterData:
     Tests for clean_data.cleanse_master_data
     """
 
-    @pytest.mark.xfail()
-    def test_empty_input(self):
+    @pytest.fixture
+    def mock_called_functions(self, mocker):
         """
-        Tests if the function can handle an empty input correct
+        Fixture to mock the functions called by cleanse_master_data
         """
+        mock_convert_fin_dates = mocker.patch(
+            "devices_rap.clean_data.convert_fin_dates", return_value="foo", autospec=True
+        )
+        mock_convert_values_to = mocker.patch(
+            "devices_rap.clean_data.convert_values_to", return_value="bar", autospec=True
+        )
+        mock_logger = mocker.patch("devices_rap.clean_data.logger")
 
+        return mock_convert_fin_dates, mock_convert_values_to, mock_logger
 
-class TestCleanseExceptionsData:
-    """
-    Tests for clean_data.cleanse_exceptions_data
-    """
-
-    @pytest.mark.xfail()
-    def test_empty_input(self):
+    @pytest.fixture
+    def test_master_df(self):
         """
-        Tests if the function can handle an empty input correct
+        Fixture to create a test master DataFrame
         """
+        return pd.DataFrame(
+            {
+                "der_high_level_device_type": ["test_der_high_level_device_type"],
+                "cln_activity_year": ["test_cln_activity_year"],
+                "cln_activity_month": ["test_cln_activity_month"],
+            }
+        )
 
-
-class TestCleanseProviderData:
-    """
-    Tests for clean_data.cleanse_provider_data
-    """
-
-    @pytest.mark.xfail()
-    def test_empty_input(self):
+    def test_covert_fin_datas_calls(self, test_master_df, mock_called_functions):
         """
-        Tests if the function can handle an empty input correct
+        Test that the function applies the convert_fin_dates function correctly
         """
+        mock_convert_fin_dates, _, _ = mock_called_functions
+        clean_data.cleanse_master_data(test_master_df)
+        assert mock_convert_fin_dates.call_count == 1
+
+    def test_convert_values_to_calls(self, test_master_df, mock_called_functions):
+        """
+        Test that the function applies the convert_values_to function correctly
+        """
+        _, mock_convert_values_to, _ = mock_called_functions
+        clean_data.cleanse_master_data(test_master_df)
+        assert mock_convert_values_to.call_count == 2
+        mock_convert_values_to.assert_called_with(
+            "test_cln_activity_year", match=[2425], to=202425
+        )
+
+    def test_logger_calls(self, test_master_df, mock_called_functions):
+        """
+        Test that the function logs the expected messages
+        """
+        _, _, mock_logger = mock_called_functions
+        clean_data.cleanse_master_data(test_master_df)
+        assert mock_logger.info.call_count == 4
+        mock_logger.info.assert_any_call("Cleaning the master dataset ready for processing")
+        mock_logger.info.assert_any_call("Converting high level device type values")
+        mock_logger.info.assert_any_call("Converting activity year values without century")
+        mock_logger.info.assert_any_call("Converting activity date values to datetime")
 
 
 if __name__ == "__main__":
