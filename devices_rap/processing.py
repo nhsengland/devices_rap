f40624a0d19a91db830d8da0314725aa9f8a142d
joseph.wilson8-nhs <joseph.wilson8@nhs.net>
2024-12-30 11:02:19 +0000

    Remove unused function for translating high level device types in processing.py as it is implemented in clean_data.py

--- devices_rap/processing.py
+++ devices_rap/processing.py
@@ -6,28 +6,6 @@ import pandas as pd
 from loguru import logger
 
 
-def translate_high_level_devices_type(
-    data: pd.DataFrame, col_name: str = "high_level_device_type"
-) -> pd.DataFrame:
-    """
-    _summary_
-
-    Parameters
-    ----------
-    data : pd.DataFrame
-        _description_
-    col_name : str, optional
-        _description_, by default "high_level_device_type"
-
-    Returns
-    -------
-    pd.DataFrame
-        _description_
-    """
-    logger.info(f"Translating the high level device type in column: {col_name}")
-    return data
-
-
 def lookup_provider_codes(master_df: pd.DataFrame, provider_codes: pd.DataFrame) -> pd.DataFrame:
     """
     _summary_
