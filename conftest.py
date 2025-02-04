c953124f9055804d14d767253997b28ae7fa6030
joseph.wilson8-nhs <joseph.wilson8@nhs.net>
2025-02-04 18:52:41 +0000

    Enhance test fixtures by adding UUID for temporary CSV files and mocking loguru logger debug method

--- conftest.py
+++ conftest.py
@@ -2,10 +2,11 @@
 This module contains fixtures that are used by the test modules in the tests/ directory.
 """
 
+from typing import Any, Dict, Literal
 import warnings
 import pandas as pd
 import pytest
-import loguru
+from uuid import uuid4
 
 from devices_rap.config import DATA_DIR
 from devices_rap.data_in.load_csv import NA_VALUES
@@ -14,14 +15,24 @@ warnings.filterwarnings("ignore", category=DeprecationWarning, message=".*platfo
 
 TEST_DATA_DIR = DATA_DIR / "_test"
 
+Logger = Any
+
+MockLoggerDict = Dict[
+    Literal["info"]
+    | Literal["error"]
+    | Literal["warning"]
+    | Literal["success"]
+    | Literal["debug"],
+    Logger,
+]
+
+
 @pytest.fixture
-def create_temp_csv_file():
+def create_temp_csv_file(tmp_path):
     """
     Fixture to create a temporary CSV file with test data, returning the file path, and then
     deleting the file and the _test directory after the test has run.
     """
-    # Create the _test directory if it doesn't exist
-    TEST_DATA_DIR.mkdir(parents=True, exist_ok=False)
 
     test_csv_data = {
         "columns": ["column_1", "column_2"],
@@ -36,20 +47,13 @@ def create_temp_csv_file():
     }
     test_csv_df = pd.DataFrame(**test_csv_data)
 
-    test_csv_file_path = TEST_DATA_DIR / "_test_data.csv"
+    test_csv_file_path = tmp_path / f"_{uuid4()}_test_data.csv"
 
     assert not test_csv_file_path.exists()
 
     test_csv_df.to_csv(test_csv_file_path, index=False)
 
-    yield test_csv_file_path
-
-    # Clean up the _test directory and its contents
-    test_csv_file_path.unlink()
-    TEST_DATA_DIR.rmdir()
-
-    assert not test_csv_file_path.exists()
-    assert not TEST_DATA_DIR.exists()
+    return test_csv_file_path
 
 
 @pytest.fixture
@@ -57,7 +61,7 @@ def mock_info(mocker):
     """
     Fixture to mock the loguru.logger.info method
     """
-    return mocker.spy(loguru.logger, "info")
+    return mocker.patch("loguru.logger.info")
 
 
 @pytest.fixture
@@ -65,7 +69,7 @@ def mock_error(mocker):
     """
     Fixture to mock the loguru.logger.error method
     """
-    return mocker.spy(loguru.logger, "error")
+    return mocker.patch("loguru.logger.error")
 
 
 @pytest.fixture
@@ -73,7 +77,7 @@ def mock_warning(mocker):
     """
     Fixture to mock the loguru.logger.warning method
     """
-    return mocker.spy(loguru.logger, "warning")
+    return mocker.patch("loguru.logger.warning")
 
 
 @pytest.fixture
@@ -81,19 +85,36 @@ def mock_success(mocker):
     """
     Fixture to mock the loguru.logger.success method
     """
-    return mocker.spy(loguru.logger, "success")
+    return mocker.patch("loguru.logger.success")
 
 
 @pytest.fixture
-def mock_log_levels(mock_info, mock_error, mock_warning, mock_success):
+def mock_debug(mocker):
+    """
+    Fixture to mock the loguru.logger.debug method
+    """
+    return mocker.patch("loguru.logger.debug")
+
+
+@pytest.fixture(autouse=True)
+def mock_log_levels(
+    mock_info, mock_error, mock_warning, mock_success, mock_debug
+) -> MockLoggerDict:
     """
     Fixture to mock the loguru.logger methods:
     - info
     - error
     - warning
     - success
+    - debug
     """
-    return mock_info, mock_error, mock_warning, mock_success
+    return {
+        "info": mock_info,
+        "error": mock_error,
+        "warning": mock_warning,
+        "success": mock_success,
+        "debug": mock_debug,
+    }
 
 
 @pytest.fixture
@@ -102,4 +123,3 @@ def empty_df():
     Fixture to return an empty DataFrame
     """
     return pd.DataFrame()
-
